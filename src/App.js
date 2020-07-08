8a9882be5cdcf4eb7b1a37d75a6485e56d6cfc80
Pole <sarmite.pole@accenture.com>
2020-07-08 23:06:29 +0300

    joke

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <h1>Piku-Peku</h1>
+        <h1>Piku-Peku JOKE !!!</h1>
         <a
           className='App-link'
           href='https://reactjs.org'
