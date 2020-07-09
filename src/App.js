46cf27a0e8f55f64768655f6a988fab162b88eb6
Janis Knets <janis.knets@accenture.com>
2020-07-09 15:01:54 +0300

    REFERENCE: React hooks example DESCRIPTION:  - added atoms folder  - added button component  - added molecules folder  - added counter compoment, which uses button and allows to increase      and decrease a set counter in this component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Counter from './molecules/counter';
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Counter />
+        <Counter />
         <a
           className='App-link'
           href='https://reactjs.org'
