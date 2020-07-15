e746e9fe9bd42fb19c434edeab19cf9b45ccfd8e
Janis Knets <janis.knets@accenture.com>
2020-07-15 14:57:17 +0300

    REFERENCE: react-router DESCRIPTION:   - added home page   - added products page   - added routing trought navigation component

---
+++ src/molecules/navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navigation () {
+  return (
+    <>
+      <Link to="/" >Home</Link>
+      <Link to="/products">Products</Link>
+    </>
+  );
+}
+
+export default Navigation;
