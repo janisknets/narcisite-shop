480778b5b21770d5354c4d347389619c85a99b58
Janis Knets <janis.knets@accenture.com>
2020-07-16 14:33:31 +0300

    REFERNCE: navigation DESCRIPTION:   - Fixed bootstrap css includes

--- src/molecules/navigation.js
+++ src/molecules/navigation.js
@@ -1,35 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
-import logot from '../logo.svg';
+import logo from '../logo.svg';
 
  function Navigation () {
     return (
       <>
-        <Navbar collapseOnSelect expand="md" bg="primary" variant="dark">
-                <Navbar.Brand href='/'>
-                    <img
-                    src={logot}
-                    height="70"
-                    width="70"
-                    className="d-inline-block align-right"
-                    alt="logo" 
-                    />
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto">
-                    <Link to="/" >Home</Link>
-                    <Link to="/products" >Products</Link>
-                </Nav>
-                <Form inline>
-                  <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                  <Button variant="outline-success">Search</Button>
-                </Form>
-          </Navbar.Collapse> 
-        </Navbar>
-      </>
-    )
-    }
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href='/'>
+              <img src={logo} height="70" width="70" alt="logo" />
+          </Navbar.Brand>
+          {/* <Navbar.Toggle aria-controls="responsive-navbar-nav" /> */}
+          {/* <Navbar.Collapse id="responsive-navbar-nav"> */}
+          <Nav className="mr-auto">
+              <Link to="/" >Home</Link>
+              <Link to="/products" >Products</Link>
+          </Nav>
+          <Nav className="mr-auto">
+              <Nav.Link href="/" >Home</Nav.Link>
+              <Nav.Link href="/products" >Products</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        {/* </Navbar.Collapse>  */}
+      </Navbar>
+    </>
+  )
+}
 
 export default Navigation;
