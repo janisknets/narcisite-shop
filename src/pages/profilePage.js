660746be52223eafb84ad174f55263005e8ef5d5
Kristina <kristina.rodzina@accenture.com>
2020-07-21 10:25:39 +0300

    ProfilePage update - Table was removed - Grid system added

--- src/pages/profilePage.js
+++ src/pages/profilePage.js
@@ -1,54 +1,63 @@
 import React from 'react';
-import { Tab, Tabs, Table } from 'react-bootstrap';
+import { Tab, Tabs, Col, Container, Row } from 'react-bootstrap';
 
 function ProfilePage () {
   return (
     <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
   <Tab eventKey="info" title="My Info">
-  <Table striped bordered hover variant="dark">
-  <thead>
-    <tr>
-      <th>Name</th>
-      <th>Surname</th>
-      <th>Address</th>
-      <th>Phone number</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>John</td>
-      <td>Doe</td>
-      <td>Dream street 7, London, UK</td>
-      <td>+ 123 222 333 44</td>
-    </tr>
-   </tbody>
-   </Table>
-  </Tab>
+    <Container>
+      <Row id="userInfo">
+        <Col sm={2}>Name Surname</Col>
+        <Col sm={2}>John Doe</Col>
+      </Row>
+      <Row id="userInfo">
+        <Col sm={2}>Address</Col>
+        <Col sm={2}>London street 1, UK</Col>
+      </Row>
+      <Row id="userInfo">
+        <Col sm={2}>Phone</Col>
+        <Col sm={2}>+111 222 333 5</Col>
+      </Row>
+    </Container>
+  </Tab> 
   <Tab eventKey="orders" title="My orders">
-  <Table striped bordered hover variant="dark">
-  <thead>
-    <tr>
-      <th>Order nr</th>
-      <th>Order date</th>
-      <th>Delivery date</th>
-      <th>Status</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>1</td>
-      <td>19-01-2015</td>
-      <td>20-01-2015</td>
-      <td>Closed</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>17-07-2020</td>
-      <td>18-07-2020</td>
-      <td>Active</td>
-    </tr>
-  </tbody>
-</Table>
+    <Container>
+        <Row id="userInfo">
+          <Col sm={2}>Order Nr</Col>
+          <Col sm={2}>1</Col>
+        </Row>
+        <Row id="userInfo">
+          <Col sm={2}>Order date</Col>
+          <Col sm={2}>20/01/2020</Col>
+        </Row>
+        <Row id="userInfo">
+          <Col sm={2}>Delivery date</Col>
+          <Col sm={2}>20/02/2020</Col>
+        </Row>
+        <Row id="userInfo">
+          <Col sm={2}>Order status</Col>
+          <Col sm={2}>Closed</Col>
+        </Row>
+      </Container>
+      <br />
+      <Container>
+        <Row id="userInfo">
+          <Col sm={2}>Order Nr</Col>
+          <Col sm={2}>2</Col>
+        </Row>
+        <Row id="userInfo">
+          <Col sm={2}>Order date</Col>
+          <Col sm={2}>15/05/2020</Col>
+        </Row>
+        <Row id="userInfo">
+          <Col sm={2}>Delivery date</Col>
+          <Col sm={2}>25/07/2020</Col>
+        </Row>
+        <Row id="userInfo">
+          <Col sm={2}>Order status</Col>
+          <Col sm={2}>In Process</Col>
+        </Row>
+      </Container>
   </Tab>
 </Tabs>
   )
