3959d82ab0637f8ea798d92f0b18b9717d1cc980
Janis Knets <janis.knets@accenture.com>
2020-07-30 14:58:41 +0300

    REFERENCE: Styled Components DESCRIPTION:   - Added examples of styled components   - Fixed missing key props in multiple places

--- src/molecules/navigation.jsx
+++ src/molecules/navigation.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Nav } from 'react-bootstrap';
+import styled from 'styled-components';
 
 function Navigation() {
+  const menu = [{
+    title: 'Home',
+    to: '/',
+  },
+  {
+    title: 'Products',
+    to: '/products',
+  },
+  {
+    title: 'Movies',
+    to: '/movies',
+  }];
+
   return (
-    <>
-      <Link to="/">Home</Link>
-      <Link to="/products">Products</Link>
-    </>
+    <Nav>
+      {
+        menu.map((object) => <StyledNavLink as={Link} to={object.to} key={object.title}>{object.title}</StyledNavLink>)
+      }
+      <Nav.Link href="/">Test</Nav.Link>
+    </Nav>
   );
 }
+
+const StyledNavLink = styled(Nav)`
+  display: block;
+  padding: .5rem 1rem;
+  background: grey;
+  color: purple;
+  
+`;
 
 export default Navigation;
